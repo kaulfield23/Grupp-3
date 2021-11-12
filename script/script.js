c5a090599e9eb84b83ced4139982b62a67584b18
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2021-11-12 22:43:22 +0100

    co worker david script update

--- script/script.js
+++ script/script.js
@@ -1,52 +1,76 @@
 //cards copy and array save TEST
           
-let user_list  = [{"challenges":1 },{"challenges":2 },{"challenges":3 },{"challenges":4 },{"challenges":5 },
-{"challenges":6 },{"challenges":7},{"challenges":8 },{"challenges":9 },{"challenges":10}, 
-{"challenges":11 },{"challenges":12 },{"challenges":13 },{"challenges":14 },{"challenges":15 },
-{"challenges":16 },{"challenges":17},{"challenges":18 },{"challenges":19 },{"challenges":20},
-{"challenges":21 },{"challenges":22 },{"challenges":23 },{"challenges":24 },{"challenges":25 },
-{"challenges":26 },{"challenges":27},{"challenges":28 },{"challenges":29 },{"challenges":30}, 
-];
 
-async function getData()
-   {
+
+
+ async function getData()
+ {
     const url = 'https://lernia-sjj-assignments.vercel.app/api/challenges';
     const response = await fetch(url);
     const data = await response.json();
-
-
-    
-    let card = data.challenges[0];
-    let type = document.querySelector(".challenges-type").innerContent=card.type;
-    let Title = document.querySelector(".challenge-title").innerContent = card.title;
-    let Description = document.querySelector(".challenge-description").innerContent = card.description;
-    let MinParticipants = document.querySelector(".minParticipants").innerContent = card.minParticipants;
-    let MaxParticipants = document.querySelector(".maxParticipants").innerContent = card.maxParticipants;
-    let Rating = document.querySelector(".challenge-rating-star").innerContent = card.rating;
-    let Image = document.querySelector(".challenge-picture").innerContent = card.image; //did we do this right? o.O
-    let Lables = document.querySelector(".challenges-lable").innerContent = card.labels;
-    let Btn = document.querySelector(".challenge-cta");
-     
    
-    if (card.type == "onsite"){
+   
+
+for (let i =0; i<data.challenges.length; i++){
+  const challenge = data.challenges[i];
+  console.log(challenge);
+   let card = document.querySelector(".challenge-all");
+    const array = []; 
+    let newCard = card.cloneNode(true);
+    card.after(newCard);
+    array.push(newCard);  
+  const promises = [];
+  
+  for(let i = 0; i<1;i++){
+    const req = fetch('https://lernia-sjj-assignments.vercel.app/api/challenges'); 
+    promises.push(req);
+  }
+  const finalData = await Promise.all(promises);
+  console.log(finalData);
+  
+    card = challenge;
+    let Type = document.querySelector(".challenges-type").innerHTML=card.type;
+    let Title = document.querySelector(".challenge-title").innerHTML = card.title;
+    let Description = document.querySelector(".challenge-description").innerHTML = card.description;
+    let MinParticipants = document.querySelector(".minParticipants").innerHTML = card.minParticipants;
+    let MaxParticipants = document.querySelector(".maxParticipants").innerHTML = card.maxParticipants;
+    let Rating = document.querySelector(".challenge-rating").innerHTML = card.rating;
+    let Image = document.querySelector(".challenge-picture").innerHTML = card.image; //did we do this right? o.O
+    let Lables = document.querySelector(".challenges-lable").innerHTML = card.labels;
+    let Btn1 = document.querySelector(".challenge-cta");
+    let Btn2 = document.querySelector(".challenge-cta");
+    
+     
+    
+    if (Type == "onsite"){
       Title = document.querySelector(".challenge-title").innerHTML = card.title + " (On-Site)";
+      Btn2.innerHTML = "Book this room";
+
+      
     }
-    else if(card.type == "online"){
+    else if(Type == "online"){
         Title = document.querySelector(".challenge-title").innerHTML = card.title;
-        Btn.innerHTML = "Take this challenge online";
+        Btn2.innerHTML = "Take this challenge online";
         
-       }
-      }  
+      }
+        
      //  document.querySelector(".challenge-rating-star").innerHTML = '*'.repeat(card.rating);
 
-  getData();
+    }
+        
+  }
+  
+   getData();
 
-  function Copy(){
-    let card = document.querySelector(".challenges-list");
-    for(let i = 0; i < 29; i++){
-            const array = []; 
-            let newCard = card.cloneNode(true);
-            card.after(newCard);
-            array.push(newCard);  
-     }
-  }  
+
+//fixa stjärnorna
+//bara visa 15 cards
+//VARFÖR GÅR DET SÅ LÅNGSAMT
+//participants??
+//bilden???
+//cards och text storlek
+//städa koden
+//Kolla om det går att filtera 
+//filter knapp
+//buttons blir lika stora
+//dölja vårt original card?
