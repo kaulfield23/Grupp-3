21d26e453bcaf568377184f862e7bc449a693054
David Kurbanov <63120629+TheAzne@users.noreply.github.com>
2021-11-16 18:28:41 +0100

    Co-authored-by: Emelie Wiberg <Erm0es@users.noreply.github.com> Co-authored-by: JohanEkstroem <JohanEkstroem@users.noreply.github.com>

--- script/script.js
+++ script/script.js
@@ -1,81 +1,54 @@
-//cards copy and array save TEST
-          
-async function getData()
-{
-   let url = 'https://lernia-sjj-assignments.vercel.app/api/challenges';
-   let response = await fetch(url);
-   let data = await response.json();
-  
-    for (let i =0; i<data.challenges.length; i++){
-    
-     console.log(data.challenges.length);
-     const challenge = data.challenges[i];
-      let card = document.querySelector(".challenge-all");
-      const array = []; 
-      let newCard = card.cloneNode(true);
-      card.after(newCard);
-      array.push(newCard);  
-      const promises = [];
-     // data.challenges[i].style.display = "none";
-    
-     
+ 
+//Function for fetching information from api
+async function getData(number){
 
-      for(let i = 0; i<1;i++){
-      const req = fetch(url); 
-      promises.push(req);
+  const url = 'https://lernia-sjj-assignments.vercel.app/api/challenges';
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data.challenges);
+    
+  //loopar cards och information
+   for (let i =0; i<number; i++){
+   let challenge = data.challenges[i];
+   let card = document.querySelector(".challenge-all");
+   let newCard = card.cloneNode(true);
+   card.after(newCard);
+    
+    
+     //ändrar värdet i deklarationerna
+    card = challenge;
+     let Type = document.querySelector(".challenges-type").innerHTML=card.type;
+     let Title = document.querySelector(".challenge-title").innerHTML = card.title;
+     let Description = document.querySelector(".challenge-description").innerHTML = card.description;
+     let MinParticipants = document.querySelector(".minParticipants").innerHTML = card.minParticipants + "-";
+     let MaxParticipants = document.querySelector(".maxParticipants").innerHTML = card.maxParticipants + " Participants";
+     let Rating = document.querySelector(".challenge-rating").innerHTML = card.rating;
+     let Image = document.querySelector(".challenge-picture").innerHTML = card.image; //did we do this right? o.O
+     let Lables = document.querySelector(".challenges-lable").innerHTML = card.labels;
+     let Btn1 = document.querySelector(".challenge-cta");
+     let Btn2 = document.querySelector(".challenge-cta");
+  
+    if (Type == "onsite"){
+      Title = document.querySelector(".challenge-title").innerHTML = card.title + "(on-site)";
+      Btn2.innerHTML = "Book this room";
       }
-      const finalData = await Promise.all(promises);
+       else if(Type == "online"){
+        Title = document.querySelector(".challenge-title").innerHTML = card.title;
+        Btn2.innerHTML = "Take this challenge online";
+    
+       }
 
-  
-      card = challenge;
-      let Type = document.querySelector(".challenges-type").innerHTML=card.type;
-      let Title = document.querySelector(".challenge-title").innerHTML = card.title;
-      let Description = document.querySelector(".challenge-description").innerHTML = card.description;
-      let MinParticipants = document.querySelector(".minParticipants").innerHTML = card.minParticipants;
-      let MaxParticipants = document.querySelector(".maxParticipants").innerHTML = card.maxParticipants;
-      let Rating = document.querySelector(".challenge-rating").innerHTML = card.rating;
-      let Image = document.querySelector(".challenge-picture").innerHTML = card.image; //did we do this right? o.O
-      let Lables = document.querySelector(".challenges-lable").innerHTML = card.labels;
-      let Btn1 = document.querySelector(".challenge-cta");
-      let Btn2 = document.querySelector(".challenge-cta");
-   
-
-      if (Type == "onsite"){
-     Title = document.querySelector(".challenge-title").innerHTML = card.title + " (On-Site)";
-     Btn2.innerHTML = "Book this room";
-     }
-      else if(Type == "online"){
-       Title = document.querySelector(".challenge-title").innerHTML = card.title;
-       Btn2.innerHTML = "Take this challenge online";
-     }
-       
-    
-    
-    
-  } //  document.querySelector(".challenge-rating-star").innerHTML = '*'.repeat(card.rating);
-    
-      
-     
-    
+  }
 }
- 
-  getData();
+getData(15);
 
- 
-
-/*
-      const slicing = data.challenges.slice(0,15);
-      if( slicing){
-        console.log(slicing);
-      } else {
-        console.log("problem");
-      }*/
-
-
+    
+//function somename(parameter1,parameter2)  
+//så parameter 1 blir data som ni hämtar från fetch och parameter 2 blir numret som ni vill loopa
 
   
 
-//fixa stjärnorna
+//fixa stjärnorna switch
 //bara visa 15 cards
 //VARFÖR GÅR DET SÅ LÅNGSAMT
 //participants??
