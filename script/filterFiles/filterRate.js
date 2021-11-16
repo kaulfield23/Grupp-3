c25f7444c553c93ac6d64b3b645e34501321b6b7
kaulfield23 <kaulfield23@gmail.com>
2021-11-16 22:19:03 +0100

    fixing rating

--- script/filterFiles/filterRate.js
+++ script/filterFiles/filterRate.js
@@ -1,44 +1,90 @@
 let minNum = 0;
 let maxNum = 10;
-const ratingFilter1 = (event) => {
-  minNum = parseInt(event.target.id);
-  if (minNum >= maxNum - 5) {
-    alert("Give a number which is less than maximum number");
-  } else {
+
+const giveStarAndGetNumbers = (event) => {
+  let secondStar = document.getElementById("2");
+  let inputNum = parseInt(event.target.id);
+  if (
+    event.target.id === "1" &&
+    event.target.className === "fas fa-star" &&
+    secondStar.className === "far fa-star"
+  ) {
+    let star = document.getElementById("1");
+    star.className = "far fa-star";
+    star.setAttribute("onlick", "ratingFilter1(event)");
+    minNum = 0;
     filterRate(minNum);
-    for (let i = 1; i < minNum + 1; i++) {
-      let star = document.getElementById(`${i}`);
-      star.setAttribute("class", "fas fa-star");
-      star.setAttribute("onlick", "ratingFilter1(event)");
-      star.style.color = "purple";
-    }
-    for (let i = minNum; i < 5; i++) {
-      let star = document.getElementById(`${i + 1}`);
-      star.setAttribute("class", "far fa-star");
-      star.style.color = "purple";
+  } else {
+    let forMin = inputNum < maxNum - 5;
+    let forMax = minNum < inputNum - 5;
+    let condition = inputNum <= 5 ? forMin : forMax;
+    if (inputNum <= 5) {
+      if (condition) {
+        minNum = parseInt(event.target.id);
+        filterRate(minNum);
+        changeStar(1, minNum);
+      } else {
+        alert("Give a number which is less than maximum number");
+      }
+    } else {
+      if (condition) {
+        maxNum = parseInt(event.target.id);
+        filterRate(maxNum);
+        changeStar(6, maxNum);
+      } else {
+        alert(`Give a number which is more than minimum number`);
+      }
     }
   }
 };
 
-const ratingFilter2 = (event) => {
-  maxNum = parseInt(event.target.id);
-  if (maxNum - 5 < minNum) {
-    alert(`Give a number which is more than minimum number`);
-  } else {
-    filterRate(maxNum);
-    for (let i = 6; i < maxNum + 1; i++) {
-      let star = document.getElementById(`${i}`);
-      star.setAttribute("class", "fas fa-star");
-      star.setAttribute("onlick", "ratingFilter1(event)");
-      star.style.color = "purple";
-    }
-    for (let i = maxNum; i < 10; i++) {
-      let star = document.getElementById(`${i + 1}`);
-      star.setAttribute("class", "far fa-star");
-      star.style.color = "purple";
-    }
+// const ratingFilter1 = (event) => {
+//   let secondStar = document.getElementById("2");
+//   if (parseInt(event.target.id) < maxNum - 5) {
+//     minNum = parseInt(event.target.id);
+//   } else {
+//     alert("Give a number which is less than maximum number");
+//   }
+//   if (
+//     event.target.id === "1" &&
+//     event.target.className === "fas fa-star" &&
+//     secondStar.className === "far fa-star"
+//   ) {
+//     let star = document.getElementById("1");
+//     star.className = "far fa-star";
+//     star.setAttribute("onlick", "ratingFilter1(event)");
+//     minNum = 0;
+//   } else {
+//     changeStar(1, minNum);
+//   }
+//   filterRate(minNum);
+//   // changeStar(1, minNum);
+// };
+// const ratingFilter2 = (event) => {
+//   if (parseInt(event.target.id) - 5 > minNum) {
+//     maxNum = parseInt(event.target.id);
+//   } else {
+//     alert(`Give a number which is more than minimum number`);
+//   }
+//   filterRate(maxNum);
+//   changeStar(6, maxNum);
+// };
+
+const changeStar = (num, bpNum) => {
+  let max = num > 5 ? 10 : 5;
+  for (let i = num; i < bpNum + 1; i++) {
+    let star = document.getElementById(`${i}`);
+    star.setAttribute("class", "fas fa-star");
+    star.setAttribute("onlick", "ratingFilter1(event)");
+    star.style.color = "purple";
+  }
+  for (let i = bpNum; i < max; i++) {
+    let star = document.getElementById(`${i + 1}`);
+    star.setAttribute("class", "far fa-star");
+    star.style.color = "purple";
   }
 };
+
 let min = 0;
 let max;
 let rate = {};
@@ -46,6 +92,11 @@ const filterRate = (num) => {
   num > 5 ? (max = num - 5) : (min = num);
   rate.min = min;
   rate.max = max;
+
+  let item = JSON.parse(JSON.stringify(cards));
   console.log(rate);
+  let filteredRate = item.filter(
+    (card) => rate.min <= card.rating && card.rating <= rate.max
+  );
 };
 filterRate(maxNum);
---
+++ script/filterFiles/printFilteredCard.js
@@ -0,0 +1,18 @@
+function printFilteredCard() {
+  let showSelectedTags = document.querySelector(".showSelectedTags");
+  let item = JSON.parse(JSON.stringify(cards));
+  let filtered = item.filter((card) => {
+    return (
+      (checkBoxFilters.length === 0 ||
+        checkBoxFilters.indexOf(card.type) !== -1) &&
+      (tagFilter.length === 0 ||
+        tagFilter.every((value) => card.labels.includes(value)))
+    );
+  });
+
+  let result = JSON.stringify(filtered);
+  if (filtered.length === 0) {
+    result = "No matching result";
+  }
+  showSelectedTags.innerHTML = result;
+}
