0ce015ce8399596ca702ea3b93b2876772b7a2c2
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2021-11-17 19:50:45 +0100

    Co-authored-by: David Kurbanov

--- script/script.js
+++ script/script.js
@@ -1,60 +1,61 @@
- 
-//Function for fetching information from api
-async function getData(number){
-
+//Function for fetching api.
+async function getData() {
   const url = 'https://lernia-sjj-assignments.vercel.app/api/challenges';
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data.challenges);
+  let card = document.querySelector(".challenge-all");
+
+  //loop for cards and information.
+  for (let i = 0; i < data.challenges.length; i++) {
+    let challenge = data.challenges[i];
+    let newCard = card.cloneNode(true);
+    card.parentNode.append(newCard);
+
+    //ändrar värdet i deklarationerna.
+    let Type = newCard.querySelector(".challenges-type").innerHTML = challenge.type;
+    let Title = newCard.querySelector(".challenge-title").innerHTML = challenge.title;
+    let Description = newCard.querySelector(".challenge-description").innerHTML = challenge.description;
+    let MinParticipants = newCard.querySelector(".minParticipants").innerHTML = challenge.minParticipants + " - ";
+    let MaxParticipants = newCard.querySelector(".maxParticipants").innerHTML = challenge.maxParticipants + " Participants";
+    let Image = newCard.querySelector(".challenge-picture").innerHTML = challenge.image; 
+    let Lables = newCard.querySelector(".challenges-lable").innerHTML = challenge.labels;
+    let Btn1 = newCard.querySelector(".challenge-cta");
+    let Btn2 = newCard.querySelector(".challenge-cta");
     
-  //loopar cards och information
-   for (let i =0; i<number; i++){
-   let challenge = data.challenges[i];
-   let card = document.querySelector(".challenge-all");
-   let newCard = card.cloneNode(true);
-   card.after(newCard);
-    
-    
-     //ändrar värdet i deklarationerna
-    card = challenge;
-     let Type = document.querySelector(".challenges-type").innerHTML=card.type;
-     let Title = document.querySelector(".challenge-title").innerHTML = card.title;
-     let Description = document.querySelector(".challenge-description").innerHTML = card.description;
-     let MinParticipants = document.querySelector(".minParticipants").innerHTML = card.minParticipants + "-";
-     let MaxParticipants = document.querySelector(".maxParticipants").innerHTML = card.maxParticipants + " Participants";
-     let Rating = document.querySelector(".challenge-rating").innerHTML = card.rating;
-     let Image = document.querySelector(".challenge-picture").innerHTML = card.image; //did we do this right? o.O
-     let Lables = document.querySelector(".challenges-lable").innerHTML = card.labels;
-     let Btn1 = document.querySelector(".challenge-cta");
-     let Btn2 = document.querySelector(".challenge-cta");
-  
-    if (Type == "onsite"){
-      Title = document.querySelector(".challenge-title").innerHTML = card.title + "(on-site)";
+    //changes cards depending on "onsite" or "online".
+    if (Type == "onsite") { 
       Btn2.innerHTML = "Book this room";
+    }
+    else if (Type == "online") {
+      Btn2.innerHTML = "Take this challenge online";
+    }
+
+    //Makes the rating number a star.
+    const starContainer = newCard.querySelector('.challenge-rating');
+    for (let starIndex = 0; starIndex < 5; starIndex++) {
+      const star = document.createElement('li');
+      if (starIndex <= (challenge.rating - 1)) {
+        star.className = 'fa fa-star';
       }
-       else if(Type == "online"){
-        Title = document.querySelector(".challenge-title").innerHTML = card.title;
-        Btn2.innerHTML = "Take this challenge online";
-    
-       }
-
+      else if (starIndex < challenge.rating) {
+        star.className = 'fa fa-star-half-alt';
+      }
+      else {
+        star.className = 'fa fa-star-o';
+      }
+      starContainer.append(star);
+    }
   }
+  //removes "original" hardcoded card.
+  card.remove();
+  
 }
-getData(15);
-
-    
-//function somename(parameter1,parameter2)  
-//så parameter 1 blir data som ni hämtar från fetch och parameter 2 blir numret som ni vill loopa
+getData();
 
   
-
-//fixa stjärnorna switch
-//bara visa 15 cards
-//VARFÖR GÅR DET SÅ LÅNGSAMT
-//participants??
 //bilden???
 //cards och text storlek
-//städa koden
-//Kolla om det går att filtera 
-//buttons blir lika stora
-//dölja vårt original card?
+
+
+
+
