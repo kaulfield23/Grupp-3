64dfe2ea890473a967d187e1d2683e62815751d9
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2021-11-17 23:09:24 +0100

    Fixed minor bugs with input spelling

--- script/filterFiles/filterInput.js
+++ script/filterFiles/filterInput.js
@@ -12,9 +12,18 @@ const getResult = () => {
             inputSearchContainer = '';
         }
         let item = JSON.parse(JSON.stringify(cards));
-        console.log(testing)
-        let filteredText = item.filter((card) => card.title.includes(inputSearchContainer) || card.description.includes(inputSearchContainer))
-        console.log(filteredText)
+        //console.log(testing)
+        //let filteredText = item.filter((card) => card.title.includes(inputSearchContainer) || card.description.includes(inputSearchContainer))
+        // let filteredText = item.filter((card) => card.title.toLowerCase().includes(inputSearchContainer) || card.description.toLowerCase().includes(inputSearchContainer))
+        // console.log(filteredText)
+
+
+        let matches = cards.filter(state => {
+            const regex = new RegExp(`^${inputSearchContainer}`, 'gi');
+            return state.title.match(regex) || state.description.match(regex);
+        });
+
+        console.log(matches);
     })
 }
 getResult();
