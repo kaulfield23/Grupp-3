029139952660ac5209eeea4f7d3d84ab3c3ea586
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2021-11-18 16:36:07 +0100

    Co-authored-by: David Kurbanov Co-authored-by: JohanEkstroem Co-authored-by: Haeju Eom

--- script/booking.js
+++ script/booking.js
@@ -2,28 +2,44 @@ let bookingStep1 = document.querySelector('.bookingStep1');
 let bookingStep2 = document.querySelector('.bookingStep2');
 let bookingStep3 = document.querySelector('.bookingStep3');
 let roomTitle = document.querySelector('.roomTitle');
+let roomTitle2 = document.querySelector('.roomTitle2');
+let submitBtn1 = document.querySelector('.submitBtnStep1');
+submitBtn1.setAttribute('onclick', 'giveName(event)');
+let card = document.querySelector(".challenge-all");
+
 async function getData() {
     const url = 'https://lernia-sjj-assignments.vercel.app/api/challenges';
     const response = await fetch(url);
     const data = await response.json();
-    return data;
+
+    /*for (let i = 0; i < data.challenges.length; i++) {
+        let challenge = data.challenges[i];
+        console.log(challenge);
+        if (triggerBooking == true) {
+            let roomTitle = card.querySelector(".roomTitle").innerHTML = data.title;
+            roomTitle.innerHTML = `Book room ${card.title}  Step 2`;
+        }
+    }*/
 }
-
-let title = [];
-
-getData().then((data) => {
-    data.challenges.forEach((card) => {
-        title.push(card.title);
-    });
-    roomTitle.innerHTML = `Book room ${title[0]}`;
+getData();
+//let title = [];
+/*getData().then((card) => {
+data.challenges.forEach((card) => {
+    title.push(card.title);
 });
+roomTitle.innerHTML = `Book room ${card.title}  Step 2`;
+roomTitle2.innerHTML = `Book room ${card.title} Step 2`;
+console.log();
+
+});*/
 
 
-// roomTitle.innerHTML = 'Book Room ' + title;
-// console.log(title);
-// let Title = document.querySelector(".challenge-title").innerContent = card.title;
+//roomTitle.innerHTML = 'Book Room: ' + challenges.title;
+
+let Title = document.querySelector(".challenge-title").innerHTML = card.title;
 
 function triggerBooking(event) {
+    let titleName = event.target.parentNode.children[1].textContent;
     let eventName = event.target.className;
     if (eventName === "challenge-cta") {
         bookingStep1.style.display = "block";
@@ -36,6 +52,11 @@ function triggerBooking(event) {
     } else {
         bookingStep3.style.display = "none";
     }
+    roomTitle.innerHTML = `Book room: ${titleName} step 1`;
 }
 
-//Notes: users input, compare with title description. If exists return it.*/
+const giveName = (event) => {
+        let titleName = event.target.parentNode.children[1].textContent;
+        roomTitle2.innerHTML = `Book room: ${titleName} step 2`;
+    }
+    //Notes: users input, compare with title description. If exists return it.*/
