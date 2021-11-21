a3811c931365634ef22018695ac2740ced88b321
Haeju Eom <kaulfield23@gmail.com>
2021-11-21 12:39:49 +0100

    fixed input and added some comments

--- script/filterFiles/printFilteredCard.js
+++ script/filterFiles/printFilteredCard.js
@@ -1,6 +1,6 @@
 function printFilteredCard(msg) {
   let showSelectedTags = document.querySelector(".showSelectedTags");
-  let item = JSON.parse(JSON.stringify(cards));
+  let item = JSON.parse(JSON.stringify(dataForFilter));
   let filtered = item.filter((card) => {
     return (
       (checkBoxFilters.length === 0 ||
@@ -10,6 +10,7 @@ function printFilteredCard(msg) {
         (tagFilter.every((value) => card.labels.includes(value)
         &&rate.min <= card.rating && card.rating <= rate.max)))
         &&(rate.min <= card.rating && card.rating <= rate.max)
+        &&(card.title.toLowerCase().includes(inputSearchContainer) || card.description.toLowerCase().includes(inputSearchContainer))
     );
   });
   let result = JSON.stringify(filtered);
