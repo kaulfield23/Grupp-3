a275b298c186d11de853cf745d40af116bdcc850
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2021-12-08 23:15:51 +0100

    Added error handling

--- script/booking.js
+++ script/booking.js
@@ -1,12 +1,10 @@
 let bookingStep1 = document.querySelector('.bookingStep1');
 let bookingStep2 = document.querySelector('.bookingStep2');
 let bookingStep3 = document.querySelector('.bookingStep3');
-const bookingStep1Content = document.querySelector('.bookingStep1Content');
-
 let roomTitle = document.querySelector('.roomTitle');
-const roomTitle2 = document.querySelector('.roomTitleStep2');
-const dateWarning = document.querySelector('.dateWarning');
+let roomTitle2 = document.querySelector('.roomTitleStep2');
 const createP = document.createElement('p');
+const bookingStep1Content = document.querySelector('.bookingStep1Content');
 
 let inputDate = document.querySelector('#inputDate');
 let search = document.querySelector('#search');
@@ -14,11 +12,23 @@ let originalUrl = 'https://lernia-sjj-assignments.vercel.app/api/booking/availab
 let selectTimes = document.querySelector('#selectTimes');
 let option = document.querySelectorAll('option');
 
+let selectParticipants = document.querySelector('#inputParticipants');
+let name = document.querySelector('#name');
+let email = document.querySelector('#email');
+let url = 'https://lernia-sjj-assignments.vercel.app/api/booking/reservations';
+let phoneInput = document.querySelector('#phoneInput');
 
 let titleName;
 let participant = [];
+let isFilled;
+console.log(participant);
 //Opens booking-modal and gets title and participants from the chosen challenge
 const giveTitleAndTrigger = (event) => {
+    let isEmpty;
+    if (isFilled === false) {
+        isFilled = true;
+        return;
+    } else {
         let eventName = event.target.className;
         if (eventName === "challenge-cta") {
             titleName = event.target.parentNode.children[1].textContent;
@@ -29,54 +39,54 @@ const giveTitleAndTrigger = (event) => {
             let partsContainer = [];
             participant = splitgetParts.concat(partsContainer);
             participant.pop();
-        }
-        //if (inputDate.value === '') {
 
-        //}
-        /*  else if (eventName === "submitBtnStep1" || eventName === 'submitBtnStep1FirstPage') {
+        } else if (eventName === "submitBtnStep1" || eventName === 'submitBtnStep1FirstPage') {
+
             bookingStep1.style.display = "none";
             bookingStep2.style.display = "block";
             participantsDropDown();
             roomTitle2.innerHTML = `Book room - ${ titleName }`;
-        } else if (eventName === "submitBtnStep2") {
-            bookingStep2.style.display = "none";
-            bookingStep3.style.display = "block";
+        } else
+        if (eventName === "submitBtnStep2") {
+            isEmpty = checkEmptyValue(isEmpty);
+            if (!isEmpty) {
+                bookingStep2.style.display = "none";
+                bookingStep3.style.display = "block";
+            }
         } else {
             bookingStep3.style.display = "none";
         }
-        */
+        return participant;
     }
-    //Checks if selectTimes has any old values and removes them then gets time slots from booking API. 
-search.addEventListener('click', (event) => {
+}
+
+
+
+//Checks if selectTimes has any old values and removes them then gets time slots from booking API. //Booking step 1
+search.addEventListener('click', () => {
     let result;
-    createP.innerHTML = 'Enter valid date';
-    bookingStep1Content.appendChild(createP);
-    let eventName = event.target.className;
-
     if (inputDate.value === '') {
-
-    } else if (eventName === "submitBtnStep1" || eventName === 'submitBtnStep1FirstPage') {
-        bookingStep1.style.display = "none";
-        bookingStep2.style.display = "block";
-        participantsDropDown();
-        roomTitle2.innerHTML = `Book room - ${ titleName }`;
-    } else if (eventName === "submitBtnStep2") {
+        isFilled = false;
+        bookingStep1.style.display = "block";
         bookingStep2.style.display = "none";
-        bookingStep3.style.display = "block";
+        bookingStep1Content.appendChild(createP);
+        createP.innerHTML = 'Enter valid date';
     } else {
-        bookingStep3.style.display = "none";
+        bookingStep1.style.display = 'none';
+        bookingStep2.style.display = 'block';
+        createP.innerHTML = '';
+        result = originalUrl.concat(inputDate.value);
+        fetch(result)
+            .then(res => res.json())
+            .then(data => renderAvailableSlots(data.slots))
     }
     if (selectTimes.length !== 0) {
         while (selectTimes.options.length > 0) {
             selectTimes.remove(0);
         }
     }
-    result = originalUrl.concat(inputDate.value);
-    fetch(result)
-        .then(res => res.json())
-        .then(data => renderAvailableSlots(data.slots))
-
     return result;
+
 })
 
 //Loops time slots from booking API with choosed date and renders data with createElement 'option'.
@@ -117,14 +127,13 @@ function participantsDropDown() {
 let postBookingBtn = document.querySelector('#postBookingBtn');
 postBookingBtn.addEventListener('click', postBooking);
 async function postBooking() {
-    const selectParticipants = document.querySelector('#inputParticipants');
-    let name = document.querySelector('#name');
-    let email = document.querySelector('#email');
-    let inputDate = document.querySelector('#inputDate');
-    let selectTimes = document.querySelector('#selectTimes');
-    let url = 'https://lernia-sjj-assignments.vercel.app/api/booking/reservations';
-    let phoneInput = document.querySelector('#phoneInput');
-
+    selectParticipants;
+    name;
+    email;
+    inputDate;
+    selectTimes;
+    url;
+    phoneInput;
     const response = await fetch(url, {
         method: 'POST',
         mode: 'cors',
@@ -146,6 +155,18 @@ async function postBooking() {
     })
     return response.json();
 }
+//Function for course 'Arbetsmetodik'
+const userInput = document.querySelectorAll('.userInput')
 
-//1. När man klickar på Sökknappen ska ett felmeddelande visas OM inget datum är angett.
-//2.
+const checkEmptyValue = (isEmpty) => {
+    isEmpty = false;
+    userInput.forEach((element) => {
+        if (element.value.length === 0) {
+            isEmpty = true;
+        }
+    })
+    if (isEmpty) {
+        alert('You must enter the input')
+    }
+    return isEmpty
+}
